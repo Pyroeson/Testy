4c4b0bdd5f18235f7a2106aa021a05e3cd8c67f3
Pyroeson <43775558+Pyroeson@users.noreply.github.com>
2018-10-06 16:44:53 +0200

    pierwszy_plik_ever

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int haslo=1453;
+
+    for(int i=0;i<=9999;i++){
+        printf("%d\n",i);
+        if(i==haslo){
+           printf("To jest liczba ktorej szukasz%d",i);
+            break;
+        }
+
+    }
+
+    return 0;
+}
